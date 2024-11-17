fe865d43f8792bdaba415cc4e05ee282c7ffed15
Syed-72 <90836890+Syed-72@users.noreply.github.com>
2024-11-17 11:32:31 +0000

    Update home.php

--- home.php
+++ home.php
@@ -295,3 +295,70 @@
 //?>
 -->
 
+
+  <div class="container back-b">
+    <div class="row pt-2 back-b">
+      <div class="col-md-12">
+        <div class="row">
+
+          <div class="col-md-3">
+
+            <h2 class="pb-1" style="border-bottom: solid 2px #66d891;"><b>About Us</b></h2>
+
+            <p> Curry Twist, where people dine, relax, laugh, chat, celebrate and entertain in our contemporary
+              surroundings! </p>
+
+          </div>
+
+          <div class="col-md-6 pb-3">
+
+            <h2 class="pb-1" style="border-bottom: solid 2px #66d891;"><b>Follow Us</b></h2>
+
+            <p class="text-center">Follow us on social media</p>
+            <p class="text-center">Get exclusive deals, offers and rewards!</p>
+
+            <div class="text-center">
+
+              <a href="https://www.facebook.com/CurryTwistRistorante/">
+                <img style="height: 40px; padding-right: 1%;" src="./images/facebook.png" alt="Facebook"></a>
+
+              <a href="https://www.instagram.com/currytwist_ristorante_indiano/">
+                <img style="height: 40px; padding-left: 1%;" src="./images/instagram.png" alt="instagram"></a>
+
+            </div>
+
+          </div>
+
+          <div class="col-md-3 pb-3">
+
+            <h2 class="pb-1" style="border-bottom: solid 2px #66d891;"><b>Halal Food</b></h2>
+
+            <center><img style="height: 120px; padding-right: 1%;" src="./images/halal.png" alt="We serve halal food">
+            </center>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container back-w">
+    <div class="row">
+      <footer class="container mt-4">
+
+        <p> Copyright &copy; CURRY TWIST <script>document.write(new Date().getFullYear())</script></p>
+
+      </footer>
+
+    </div>
+  </div>
+
+  <!╌ scripts to load jquery and bootstrap javascript ╌>
+    <script type="text/javascript" src=".\node_modules\jquery\dist\jquery.min.js"></script>
+    <script type="text/javascript" src=".\node_modules\bootstrap\dist\js\bootstrap.min.js"></script>
+    <script src="./node_modules/lightbox2/dist/js/lightbox.min.js"></script>
+
+</body>
+
+</html>
