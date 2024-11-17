cd0bdd44c27a3b398b89686a7dab5fabf0f17c6c
Syed-72 <90836890+Syed-72@users.noreply.github.com>
2024-11-17 11:29:42 +0000

    Add files via upload

---
+++ home.php
@@ -0,0 +1,149 @@
+
+
+  <div class="container back-b">
+
+    <div class="row">
+      <div class="col-md-12">
+        <div class="recipe-card">
+          <div class="recipe-card-image" style="background-image: url('./images/home-main.jpg');">
+
+            <div class="d-grid gap-2 col-md-12"
+              style="display:flex; height: 30rem; align-content: center; justify-content: center; padding-left: 1%; padding-right: 1%;">
+
+
+              <h1 class="mb-2 " style="text-shadow: #000 0px 0px 5px;"><b>The Best Ethnic Cuisine</b></h1>
+
+              <h2 class="mb-2 " style="text-shadow: #000 0px 0px 5px;"><b>Join us and enjoy your dinner in a friendly environment with delicious
+                ethnic dishes</b></h2>
+
+              <div class="row">
+
+                <div class="d-grid gap-2 col-md-3 mx-auto">
+
+                </div>
+
+                <div class="d-grid gap-2 col-md-3 mx-auto pb-2">
+
+                  <a href="index.php?p=reservations" class="btn btn-studenteat" style="color: white;">Reservations</a>
+
+                </div>
+
+
+                <div class="d-grid gap-2 col-md-3 mx-auto pb-2">
+
+                  <a href="./contents/Currt-Twist-Menu-06-05-22.pdf" class="btn btn-studenteat" style="color: white;">Our Menu</a>
+
+                </div>
+
+                <div class="d-grid gap-2 col-md-3 mx-auto">
+
+                </div>
+
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container back-b">
+    <div class="container back-dg">
+      <div class="row">
+        <div class="col-md-6 text-center">
+          <p><br></p>
+
+          <h1><b>Come visit us!</b></h1>
+
+          <p class="text-center"><i class="fa-solid fa-map-location-dot"></i><b> Via Carlo Farini 37, Milano</b></p>
+          <p class="text-center"><i class="fa-solid fa-square-phone"></i><a style="color: #fff; text-decoration: none" href="tel: +39 026887336"> <b> +39 026887336</b></a></p>
+          <p class="text-center"><i class="fa-solid fa-mobile-button"></i><a style="color: #fff; text-decoration: none" href="tel: +39 3293524999"> <b> +39 3293524999</b></a></p>
+          <p class="text-center"><i class="fa-solid fa-envelope"></i><a style="color: #fff; text-decoration: none" href="mailto: info@currytwist.it"> <b> info@currytwist.it</b></a></p>
+        </div>
+
+        <div class="col-md-6">
+          <figure class="text-center pt-3"><a
+              href="https://www.google.com/maps/place/Curry+Twist+Ristorante+Indiano/@45.4897676,9.1829665,15z/data=!4m5!3m4!1s0x0:0xed33364d3f1dfd77!8m2!3d45.4897676!4d9.1829665"
+              target="_blank" rel="noopener"><img decoding="async" width="600" height="300" src="./images/map.png"
+                alt="Click for directions" srcset="./images/map.png 977w, ./images/map.png 300w, ./images/map.png 768w"
+                sizes="(max-width: 977px) 100vw, 977px"></a></figure>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-12">
+
+        <h1 class="mb-4 pt-4 text-center"><b>Our Photo Gallery</b></h1>
+
+        <h2 class="mb-4 pb-2 text-center">Get inspired and get ready!</h2>
+
+      </div>
+    </div>
+
+    <div class="container back-dg">
+      <div class="row pt-3">
+        <div class="col-md-12">
+          <div class="row">
+
+            <div class="col-md-3">
+              <div class="recipe-image mb-4" style="background-image: url('./images/home-tandoor.jpeg');">
+                <a href="./images/home-tandoor.jpeg" data-lightbox="recipe-imgs"></a>
+              </div>
+            </div>
+
+            <div class="col-md-3">
+              <div class="recipe-image mb-4" style="background-image: url('./images/menu-tandoorimixplatter.jpeg');">
+                <a href="./images/menu-tandoorimixplatter.jpeg" data-lightbox="recipe-imgs"></a>
+              </div>
+            </div>
+
+            <div class="col-md-3">
+              <div class="recipe-image mb-4" style="background-image: url('./images/home-kebab.jpeg');">
+                <a href="./images/home-kebab.jpeg" data-lightbox="recipe-imgs"></a>
+              </div>
+            </div>
+
+            <div class="col-md-3">
+              <div class="recipe-image mb-4" style="background-image: url('./images/home-ChickenTikka.jpg');">
+                <a href="./images/home-ChickenTikka.jpg" data-lightbox="recipe-imgs"></a>
+              </div>
+            </div>
+
+            <div class="col-md-3">
+              <div class="recipe-image mb-4" style="background-image: url('./images/home-tandoorchick.jpeg');">
+                <a href="./images/home-tandoorchick.jpeg" data-lightbox="recipe-imgs"></a>
+              </div>
+            </div>
+
+            <div class="col-md-3">
+              <div class="recipe-image mb-4" style="background-image: url('./images/home-curries.jpeg');">
+                <a href="./images/home-curries.jpeg" data-lightbox="recipe-imgs"></a>
+              </div>
+            </div>
+
+            <div class="col-md-3">
+              <div class="recipe-image mb-4" style="background-image: url('./images/home-papdom.jpeg');">
+                <a href="./images/home-papdom.jpeg" data-lightbox="recipe-imgs"></a>
+              </div>
+            </div>
+
+            <div class="col-md-3">
+              <div class="recipe-image mb-4" style="background-image: url('./images/home-naan.jpg');">
+                <a href="./images/home-naan.jpg" data-lightbox="recipe-imgs"></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<! --
+// <?php
+//echo '<script type="text/javascript">
+//       window.onload = function () { alert("Alert!, \n \nWe are fully booked for 7th, 8th, 9th, and 10th October. \nSiamo al completo per il 7, 8, 9 e 10 ottobre."); } 
+//</script>'; 
+//?>
+-->
+
